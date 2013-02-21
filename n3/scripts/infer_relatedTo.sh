34ea8afd48ca1170e7cffe75fe3a9fc0aca15d05
Andrae Muys <andrae.muys@gmail.com>
2013-02-21 19:11:15 +1000

    Fixed numerous bugs in relatedTo inferencing

--- n3/scripts/infer_relatedTo.sh
+++ n3/scripts/infer_relatedTo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/euler/eye --yap --nope rules/*.n3 ../owl/*ArchitectureOntology.ttl omekaexport/latest.ttl entities/*.ttl classification/*.n3 pseudoentities/*.n3 inference/preferredImage.n3 --query inference/relatedTo.n3 > ont-output/relatedTo.ttl
+/opt/euler/eye --yap --nope rules/*.n3 ../owl/*ArchitectureOntology.ttl omekaexport/latest.ttl entities/*.ttl classification/*.n3 pseudoentities/*.n3 inference/preferredImage.n3 inference/relatedTo.n3 --query inference/relatedTo.n3 > ont-output/relatedTo.ttl
