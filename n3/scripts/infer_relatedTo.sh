294193086579938d1cf8f983134f6ecbf2a457bb
Andrae Muys <andrae.muys@gmail.com>
2013-02-27 13:17:32 +1000

    Updated inference results

--- n3/scripts/infer_relatedTo.sh
+++ n3/scripts/infer_relatedTo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/euler/eye --yap --nope rules/*.n3 ../owl/inferred/ArchitectureOntologyInferred.ttl omekaexport/latest.ttl resources/*.ttl classification/*.n3 pseudoentities/*.n3 inference/preferredImage.n3 inference/relatedTo.n3 --query inference/relatedTo.n3 > relatedto/relatedTo.ttl
+/opt/euler/eye --yap --nope rules/*.n3 ../owl/inferred/ArchitectureOntologyInferred.ttl omekaexport/latest.ttl resources/*.ttl classification/*.n3 pseudoentities/*.n3 inference/*.n3 --query inference/relatedTo.n3 > relatedto/relatedTo.ttl
