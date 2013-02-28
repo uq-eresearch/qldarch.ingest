f8bf9c1ad8cfbe79d91d78bb355c84356a66e211
Andrae Muys <andrae.muys@gmail.com>
2013-02-28 16:07:14 +1000

    Fresh export from omeka

--- d2rq/clean-omeka.sh
+++ d2rq/clean-omeka.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Experience indicates that some turtle parsers are intolerant of the output of d2rq due to misparsing numbers terminated with '.'.
+# Passing the output of the omeka export through rapper produces 'generous' turtle that does not trigger these bugs.
+
 cat latest-export.ttl | rapper -r -i turtle -o turtle - 'http://qldarch.net/omeka/items/show/' > latest-export-clean.ttl
--- d2rq/export-omeka.sh
+++ d2rq/export-omeka.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+mysql -u omeka omeka < default-portrait-types.sql
 cat omeka-mapping.ttl mysql.password > omeka-mapping-with-password.ttl
 ${D2RQ_HOME}/dump-rdf -f TURTLE -o latest-export.ttl omeka-mapping-with-password.ttl
