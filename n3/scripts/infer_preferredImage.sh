946285ccb18afa99b58e55bd6e92ce01ba2c73c3
Andrae Muys <andrae.muys@gmail.com>
2013-03-13 16:08:04 +1000

    Fixed export scripts.

--- n3/scripts/infer_preferredImage.sh
+++ n3/scripts/infer_preferredImage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/euler/eye --yap --nope rules/*.n3 ../owl/inferred/ArchitectureOntologyInferred.ttl omekaexport/latest.ttl resources/*.ttl classification/*.n3 pseudoentities/*.n3 --query inference/preferredImage.n3 > resources/preferredImages.ttl
+/opt/euler/eye --yap --nope rules/*.n3 ../owl/inferred/ArchitectureOntologyInferred.ttl entities/*.ttl omekaexport/latest.ttl resources/*.ttl classification/*.n3 pseudoentities/*.n3 --query inference/preferredImage.n3 > resources/preferredImages.ttl
