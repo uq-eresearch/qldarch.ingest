da474a82b7ad5fab8746241b50dcf5397b5c641e
Andrae Muys <andrae.muys@gmail.com>
2013-05-01 16:30:01 +1000

    Added export of geodata from omeka

---
+++ n3/scripts/infer_latlong.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/opt/euler/eye --yap --nope rules/*.n3 ../owl/inferred/ArchitectureOntologyInferred.ttl entities/*.ttl omekaexport/latest.ttl resources/*.ttl classification/*.n3 pseudoentities/*.n3 --query inference/infer_latlong.n3 > resources/preferredImages.ttl
